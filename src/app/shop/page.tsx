698682712b8147dd6a372efdcb9debcf19505946
eyubkh <eyub.kh@gmail.com>
2024-02-15 19:40:09 +0000

    safety commit

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -68,7 +68,14 @@ function Page() {
 												</span>
 												<button
 													type="button"
-													onClick={() => addProductHandler(product)}
+													onClick={(event) => {
+														addProductHandler(product);
+														const target = event.target as HTMLButtonElement;
+														target.innerText = "Added!";
+														setTimeout(() => {
+															target.innerText = "Add to cart";
+														}, 1000);
+													}}
 													className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 												>
 													Add to cart
